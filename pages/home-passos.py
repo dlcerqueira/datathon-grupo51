a5181b274af54ebbd5e3c96c2378f6c5d17283fd
Diego Lima <66536050+dlcerqueira@users.noreply.github.com>
2024-03-24 21:26:57 -0300

    Add files via upload

---
+++ pages/home-passos.py
@@ -0,0 +1,53 @@
+
+
+import streamlit as st
+
+
+
+colunas_1 = st.columns(2, gap="large")
+with colunas_1 [0]:
+
+             st.markdown("""A Passos Mágicos tem como objetivo acelerar a ascensão social de crianças e jovens do município de Embu Guaçu através da educação. 
+            Fornecendo aulas de português, matemática e inglês três vezes na semana, 
+            atividades extracurriculares de finais de semana e bolsas de estudos no colégio particular, em cursos técnicos e de graduação..""")
+
+with colunas_1 [1]:
+            st.image(""images\Ong-Passos Mágicos.jpeg")
+
+colunas_2 = st.columns(2, gap="large")
+with colunas_2 [0]:
+            st.image(""images\foto-passos-2.jpg")
+            
+
+with colunas_2 [1]:
+            st.markdown("""A Passos Mágicos baseia-se na meritocracia, por isso engajamos nossas crianças para que acreditem que estudar é bom e pode transformar a vida delas, 
+            dando espaço para que as mesmas busquem a mudança. Avaliamos a participação, 
+            desempenho e presença para decidir quem participará das atividades de finais de semana e quem pode concorrer a bolsas de estudos na escola particular sendo apadrinhado por uma pessoa que se dispõe a financiar seu estudo. 
+            Hoje atendemos 400 crianças, sendo que 80 estão na escola particular União, em Embu Guaçu. 
+            As aulas são dadas fora do horário de aula escolar em locais fixos cedidos pela comunidade (em 4 núcleos diferentes), ou seja, 
+            não temos uma sede construída, pois acreditamos que todo o valor de arrecadação do projeto deve ir para a melhora da qualidade de entrega e investimento em novas oportunidades para as crianças.""")
+
+st.markdown("""A Passos conta  com um corpo docente composto por 7 professores, uma psicóloga e uma psicopedagoga, 
+            todos eles contratados e remunerados para garantir a qualidade do ensino. 
+            A ong conta com inumeras atividades de final de semana podem ser culturais como visita a museus e teatros ou educacionais como oficinas,
+             cursos e workshops, todas realizadas com a ajuda de voluntários que acompanham e participam com nossas crianças.""")
+
+st.markdown("""A Passos conta  com um corpo docente composto por 7 professores, uma psicóloga e uma psicopedagoga, 
+            todos eles contratados e remunerados para garantir a qualidade do ensino. 
+            A ong conta com inumeras atividades de final de semana podem ser culturais como visita a museus e teatros ou educacionais como oficinas,
+             cursos e workshops, todas realizadas com a ajuda de voluntários que acompanham e participam com nossas crianças.""")
+
+
+colunas_3 = st.columns(2, gap="large")
+with colunas_3 [0]:
+        
+            st.markdown( """"O Gráfico ao lado demostra o números de alunos impactados pelo 
+                        programa de aceleração de crescimento da Passos.""")
+            
+            
+colunas_3 = st.columns(2, gap="large")
+with colunas_3 [1]:
+            st.image(""images\alunos-pac.jpg")
+
+
+
